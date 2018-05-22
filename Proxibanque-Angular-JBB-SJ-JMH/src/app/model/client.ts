8b163ac6b3b1615e3b97467b8ec49429e47dc955
JM. Hiltbrunner <jm.hiltbrunner@gmail.com>
2018-05-22 16:36:39 +0200

    ajout des derniers éléments métiers (compte, carte) et suite de formulaire client

--- Proxibanque-Angular-JBB-SJ-JMH/src/app/model/client.ts
+++ Proxibanque-Angular-JBB-SJ-JMH/src/app/model/client.ts
@@ -1,18 +1,52 @@
 import { Person } from "./person";
+import { CompteCourant } from "./comptecourant";
+import { CompteEpargne } from "./compteepargne";
 
-export class Client implements Person{
+export class Client implements Person {
     id: number;
     nom: string;
     prenom: string;
-    adresse ?: string;
-    ville ?: string;
-    codePostal ?: string;
-    telephone ?: string;
+    adresse: string;
+    ville: string;
+    codePostal: string;
+    telephone: string;
+    compteCourant: CompteCourant;
+    compteEpargne: CompteEpargne;
 
-    constructor(idClient: number, nomClient: string, prenomClient: string){
-        this.id=idClient;
-        this.nom=nomClient;
-        this.prenom=prenomClient;
+    constructor(options: {
+        id: number;
+        nom: string;
+        prenom: string;
+        adresse?: string;
+        ville?: string;
+        codePostal?: string;
+        telephone?: string;
+        compteCourant ?: CompteCourant;
+        compteEpargne ?: CompteEpargne;
+    }) {
+        this.id = options.id || null;
+        this.nom = options.nom || '';
+        this.prenom = options.prenom || '';
+        this.adresse = options.adresse || '';
+        this.ville = options.ville || '' ;
+        this.codePostal = options.codePostal || '';
+        this.telephone = options.codePostal || '';
+        this.compteCourant = options.compteCourant || null;
+        this.compteEpargne= options.compteEpargne || null;
+    }
+
+    toJson() {
+        return {
+          id: this.id,
+          nom: this.nom,
+          prenom: this.prenom,
+          adresse: this.adresse,
+          ville: this.ville,
+          codePostal: this.codePostal,
+          telephone: this.telephone,
+          compteCourant: this.compteCourant,
+          compteEpargne: this.compteEpargne
+        };
     }
 
 }
