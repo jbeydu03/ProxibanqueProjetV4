169940abb0aad7e7063f4295fe72e57982d35c18
jbblanc <jbblanc.pro@gmail.com>
2018-05-23 17:41:19 +0200

    liason cookie

--- Proxibanque-Angular-JBB-SJ-JMH/src/app/conseiller/conseiller.service.ts
+++ Proxibanque-Angular-JBB-SJ-JMH/src/app/conseiller/conseiller.service.ts
@@ -5,12 +5,13 @@ import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import { Client } from '../model/client';
+import { DOCUMENT } from '@angular/common';
 
 
 @Injectable()
 export class ConseillerService {
 
-  constructor(private http: HttpClient,
+  constructor(@Inject(DOCUMENT) private document,private http: HttpClient,
     @Inject('JSON_SERVER_URL') private baseUrl: string) { }
 
   loadClients(): Observable<Client[]> {
@@ -24,17 +25,31 @@ export class ConseillerService {
   }
 
   saveClient(client: Client): Observable<Client> {
+    const idConseiller = this.getCookie();
     // Si le client existe => Update et sinon => Create
     if (client.id) { // UPDATE
       // TODO: Ne pas oublier de modifier l'Id conseiller
-      return this.http.put<Client>('http://localhost:8080/ProxiBanqueSI_JMH_JBB_SJ/clients/conseiller/2/' , client);
+      return this.http.put<Client>('http://localhost:8080/ProxiBanqueSI_JMH_JBB_SJ/clients/conseiller/' + idConseiller + '/', client);
     } else { // INSERT
-      return this.http.post<Client>('http://localhost:8080/ProxiBanqueSI_JMH_JBB_SJ/clients/conseiller/2/', client);
+      return this.http.post<Client>('http://localhost:8080/ProxiBanqueSI_JMH_JBB_SJ/clients/conseiller/'+ idConseiller + '/', client);
     }
   }
 
   deleteClient(clientId: number): Observable<any> {
     return this.http.delete('http://localhost:8080/ProxiBanqueSI_JMH_JBB_SJ/clients/' + clientId);
+  }
+
+
+  getCookie(name: string = ''): string {
+    const allCookiesString = this.document.cookie;
+    const index1 = allCookiesString.indexOf(name);
+    if (index1 !== -1) {
+      let index2 = allCookiesString.indexOf(';', index1);
+      index2 = index2 === -1 ? allCookiesString.length : index2;
+      const cookieString = allCookiesString.slice(index1, index2);
+      return cookieString.split('=')[1];
+    }
+
   }
 
 }
